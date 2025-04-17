cad5f04f6c083f5089cd8ae6ea9205d0f675d69e
Julia Weppler <jnweppler@gmail.com>
2025-04-17 12:09:34 -0400

    adjusting routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-  import HomePage from './pages/HomePage';
-  import Scale from './pages/Scale';
-  import Map from './pages/Map';
-  import Forum from './pages/Forum';
-  import NewPost from './pages/NewPost';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Scale from './pages/Scale';
+import Map from './pages/Map';
+import Forum from './pages/Forum';
+import NewPost from './pages/NewPost';
 
- 
-
- 
-  function App() {
-    return (
-      +    <Router basename={process.env.PUBLIC_URL}>
-             <Routes>
-               <Route path="/" element={<HomePage />} />
-               <Route path="/scale" element={<Scale />} />
-               <Route path="/map" element={<Map />} />
-               <Route path="/forum" element={<Forum />} />
-               <Route path="/new-post" element={<NewPost />} />
-             </Routes>
-           </Router>
-         );
-       }
-       export default App;
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/scale" element={<Scale />} />
+      <Route path="/map" element={<Map />} />
+      <Route path="/forum" element={<Forum />} />
+      <Route path="/new-post" element={<NewPost />} />
+    </Routes>
+    );
+}
+export default App;
