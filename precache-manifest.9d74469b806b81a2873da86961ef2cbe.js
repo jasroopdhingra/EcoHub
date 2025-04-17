352024025ca28650dd3918e125432b5849d06df6
julia-weppler-1 <julia-weppler-1@users.noreply.github.com>
2025-04-17 15:55:51 +0000

    deploy: 41a6c155828641f41d44eb269e4465e6c7f81b81

---
+++ precache-manifest.9d74469b806b81a2873da86961ef2cbe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e90be868ae2e6f82e4f918acafe025d6",
+    "url": "/ecohub/index.html"
+  },
+  {
+    "revision": "bc89053aedbd3afc9379",
+    "url": "/ecohub/static/css/2.5470235e.chunk.css"
+  },
+  {
+    "revision": "ca0eaea5fdb009cd4f0c",
+    "url": "/ecohub/static/css/main.c854c7d2.chunk.css"
+  },
+  {
+    "revision": "bc89053aedbd3afc9379",
+    "url": "/ecohub/static/js/2.ae60b879.chunk.js"
+  },
+  {
+    "revision": "2e7f1faba63d80a99bdeac6fd99a0776",
+    "url": "/ecohub/static/js/2.ae60b879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33265c2486f474d3e878",
+    "url": "/ecohub/static/js/3.d67e1488.chunk.js"
+  },
+  {
+    "revision": "ca0eaea5fdb009cd4f0c",
+    "url": "/ecohub/static/js/main.fd8475d5.chunk.js"
+  },
+  {
+    "revision": "0b93adec90552f2355d5",
+    "url": "/ecohub/static/js/runtime-main.85a296e2.js"
+  },
+  {
+    "revision": "a67a336e398a5dc0ba906a547594a43c",
+    "url": "/ecohub/static/media/green-buildings.a67a336e.jpeg"
+  }
+]);
